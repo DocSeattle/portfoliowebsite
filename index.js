9e6d60c554fb28c978be771b2cdd8fdefa3c3a90
Seattle <seattledraws@gmail.com>
2022-06-24 13:09:45 +0200

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import '../css/main.css';
 import * as THREE from 'htts://unpkg.com/three@0.141.0/build/three.module.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 /** VARIABLES */
 let pWidth = 200;
